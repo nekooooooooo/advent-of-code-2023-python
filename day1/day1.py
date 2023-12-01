71bef74eb8f905f362e39ec2a69466d290f6ee13
nekooooooooo <nekowalkthrough@gmail.com>
2023-12-01 16:39:22 +0800

    Add second half

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,5 @@
 from aocd import get_data
+import regex
 
 lines = get_data(day=1, year=2023).splitlines()
 
@@ -9,4 +10,29 @@ for line in lines:
     calibration_value = int(f'{digit[0]}{digit[-1]}')
     calibration_values.append(calibration_value)
 
-print(sum(calibration_values))
+print(f'First Half: {sum(calibration_values)}')
+
+# second half
+
+calibration_values = []
+
+str_to_digit = {
+    'one': "1",
+    'two': "2",
+    'three': "3",
+    'four': "4",
+    'five': "5",
+    'six': "6",
+    'seven': "7",
+    'eight': "8",
+    'nine': "9",
+}
+
+for line in lines:
+    nums = regex.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line, overlapped=True)
+    digit = [str_to_digit.get(item, item) for item in nums]
+    if digit:
+        calibration_value = int(f'{digit[0]}{digit[-1]}')
+        calibration_values.append(calibration_value)
+    
+print(f'Second Half: {sum(calibration_values)}')
