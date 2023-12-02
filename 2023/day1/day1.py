24842cf461e44ea3dd40bc42c093d77972747b28
nekooooooooo <nekowalkthrough@gmail.com>
2023-12-02 11:09:08 +0800

    Refactor: updated variable names, Add: second solution

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -14,25 +14,42 @@ print(f'First Half: {sum(calibration_values)}')
 
 # second half
 
-calibration_values = []
+total = 0
 
-str_to_digit = {
-    'one': "1",
-    'two': "2",
-    'three': "3",
-    'four': "4",
-    'five': "5",
-    'six': "6",
-    'seven': "7",
-    'eight': "8",
-    'nine': "9",
+word_to_digit = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
 }
+
+import time
+
+start_time = time.time()
+
+total = 0
 
 for line in lines:
     nums = regex.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line, overlapped=True)
-    digit = [str_to_digit.get(item, item) for item in nums]
-    if digit:
-        calibration_value = int(f'{digit[0]}{digit[-1]}')
-        calibration_values.append(calibration_value)
-    
-print(f'Second Half: {sum(calibration_values)}')
+    digits = [word_to_digit.get(num, num) for num in nums]
+    if digits:
+        calibration_value = int(digits[0] + digits[-1])
+        total += calibration_value
+
+print(f'Second Half 1: {total}')
+
+total = 0
+
+for line in lines: # getting the first and last digit then converting
+    nums = regex.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line, overlapped=True)
+    first_digit = word_to_digit.get(nums[0], nums[0])
+    last_digit = word_to_digit.get(nums[-1], nums[-1])
+    calibration_value = int(first_digit + last_digit)
+    total += calibration_value
+
+print(f'Second Half 2: {total}')
